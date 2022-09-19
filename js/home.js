813e1882974a108b7bb7c3c5267818b16da40996
Flávio Ferreira <63963975+LDKid@users.noreply.github.com>
2022-09-19 18:57:47 +0100

    Version 0.2.1 - Bug Fixes
    Fixed the following bugs:
    - Navbar hidden arroe on some deviced;
    - Navbar scroll;
    - Some text was bold;
    - Can't change color on mobile;
    - Change color with arroe;
    - Home text too bug;
    - Obrigado = 404;
    - Open links in seperate page
    - Added .xcf to gitignore

--- js/home.js
+++ js/home.js
@@ -1,19 +1,36 @@
 var holdingClick = false;
 
-['mousedown', 'touchdown'].forEach( evt => document.addEventListener( evt, function (_event) { holdingClick = true; }, false ) );
-['mouseup', 'touchup'].forEach( evt => document.addEventListener( evt, function (_event) { holdingClick = false; }, false ) );
+document.addEventListener( "mousedown", function (_event) { holdingClick = true; }, false );
+document.addEventListener( "mouseup", function (_event) { holdingClick = false; }, false );
 
+for (let i = 0; i < 255; i++) {
+    let hueEl = document.getElementById(`hue-${i}`);
+    [
+        "touchstart"
+    ].forEach( evt => {
+        hueEl.addEventListener( evt, function ( _event ) {
+            clickHue(i, true)
+        }, false );
+    });
+}
 
-
-
-function clickHue(hue) {
-    if ( holdingClick ) {
-        document.querySelector('.picked-hue').classList.remove('picked-hue');
-        document.body.style.setProperty('--accent-color', `hsl(${hue},97%,53%)`);
-        document.getElementById(`hue-${hue}`).classList.add('picked-hue');
-        sessionStorage.setItem('theme', hue);
-        document.body.classList.remove('ee1');
+document.addEventListener("touchmove", (event) => {
+    var myLocation = event.touches[0];
+    var realTarget = document.elementFromPoint(myLocation.clientX, myLocation.clientY);
+    if (realTarget.dataset.component == 'color-picker-hue') {
+        clickHue(realTarget.dataset.hue, true)
     }
+});
+
+
+function clickHue( hue, force = false ) {
+    if ( !force && !holdingClick ) return;
+    
+    document.querySelector('.picked-hue').classList.remove('picked-hue');
+    document.body.style.setProperty('--accent-color', `hsl(${hue},97%,53%)`);
+    document.getElementById(`hue-${hue}`).classList.add('picked-hue');
+    sessionStorage.setItem('theme', hue);
+    document.body.classList.remove('ee1');
 
     if ( hue === 30 ) {
         setTimeout( function () {
